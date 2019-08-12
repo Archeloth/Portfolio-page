a3f70d7a3e713957a234a2155b38ac151ca6350e
Radován Barnabás <r.barnabas96@gmail.com>
2019-08-12 20:15:20 +0200

    Style update + SVG

---
+++ script.js
@@ -0,0 +1,13 @@
+const hamburger = document.getElementById('hamburger');
+const dropdownMenu = document.getElementById('dropdown');
+let isClicked = false;
+
+hamburger.addEventListener('click', ()=>{
+    if(isClicked===false){
+        isClicked=true;
+        dropdownMenu.style.right = "0";
+    }else{
+        isClicked=false;
+        dropdownMenu.style.right = "-100%";
+    }
+});
